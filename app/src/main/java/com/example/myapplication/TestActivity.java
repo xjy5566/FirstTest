e9328bff3d1cae84dba82e317b6534de7437dfc8
xjy5566 <244551717@qq.com>
2020-06-15 23:13:04 +0800

    新增一个测试类

---
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 测试类
+ */
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
